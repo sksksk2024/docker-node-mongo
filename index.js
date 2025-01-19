cffffe126930d4e1ebff48e57dfd5248c3eeb8d7
Alex <cota8091@gmail.com>
2025-01-19 17:19:00 +0200

    deployment for mongo to work

--- index.js
+++ index.js
@@ -9,27 +9,27 @@ app.use(express.urlencoded({ extended: false }));
 
 // Connect to MongoDB
 mongoose
-  .connect(
-    'mongodb://mongo:27017/docker-node-mongo',
-    { useNewUrlParser: true }
-  )
+  .connect(process.env.MONGO_URL || 'mongodb://mongo:27017/docker-node-mongo', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log(err));
+  .catch((err) => console.log(err));
 
 const Item = require('./models/Item');
 
 app.get('/', (req, res) => {
   Item.find()
-    .then(items => res.render('index', { items }))
-    .catch(err => res.status(404).json({ msg: 'No items found' }));
+    .then((items) => res.render('index', { items }))
+    .catch((err) => res.status(404).json({ msg: 'No items found' }));
 });
 
 app.post('/item/add', (req, res) => {
   const newItem = new Item({
-    name: req.body.name
+    name: req.body.name,
   });
 
-  newItem.save().then(item => res.redirect('/'));
+  newItem.save().then((item) => res.redirect('/'));
 });
 
 const port = 3000;
